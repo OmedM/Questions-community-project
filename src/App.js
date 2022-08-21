2508c0b552d44bf805a7f95b933b0bf85c396ba2
OmedM <110693726+OmedM@users.noreply.github.com>
2022-08-21 12:03:56 +0300

    MUI installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
+import Sidebar from "./components/sidebar.js";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
-    <h1>React</h1>
+    <>
+      <Sidebar />
+    </>
+
   );
 }
 
---
+++ src/components/sidebar.js
@@ -0,0 +1,10 @@
+
+
+function Sidebar() {
+
+    return(
+        <h1>Questions Community</h1>
+    )
+}
+
+export default Sidebar;
