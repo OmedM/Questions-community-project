549992423cbddf13fd3f68ba069201bdc1c90a47
OmedM <110693726+OmedM@users.noreply.github.com>
2022-08-22 10:52:21 +0300

    React router dom installed, UI in progress

---
+++ src/components/sidebar-items.js
@@ -0,0 +1,18 @@
+import QuestionAnswerIcon from '@mui/icons-material/QuestionAnswer';
+
+const sidebarItems = [
+    {
+        name: 'Questions',
+        icon: <QuestionAnswerIcon />,
+    },
+    {
+        name: 'Questions',
+        icon: <QuestionAnswerIcon />,
+    },
+    {
+        name: 'Questions',
+        icon: <QuestionAnswerIcon />,
+    },
+]
+
+export default sidebarItems;
--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,28 @@
-
+import React from 'react';
+import sidebarItems from './sidebar-items.js';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Container from '@mui/material/Container';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function Sidebar() {
 
     return(
-        <h1>Questions Community</h1>
+        <div>
+            <Container>
+                <ButtonGroup
+                    variant='outlined'
+                    color='secondary'
+                    disableElevation
+                    onClick={() => { alert('Clicked!')}}
+                >
+                    <Button startIcon={<CheckCircleIcon />} variant='contained'>Submit</Button>
+                    <Button startIcon={<DeleteIcon />}>Discard</Button>
+                </ButtonGroup>
+            </Container>
+        </div>
     )
 }
 
